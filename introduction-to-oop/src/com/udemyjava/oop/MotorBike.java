34db4d7e1283008f5452aa71d01313adf9cb7e3b
takityaki <tataki26@gmail.com>
2022-08-16 23:08:31 +0900

    Create MotorBike and MotorBikeRunner class

---
+++ introduction-to-oop/src/com/udemyjava/oop/MotorBike.java
@@ -0,0 +1,6 @@
+package com.udemyjava.oop;
+
+public class MotorBike
+{
+
+}
---
+++ introduction-to-oop/src/com/udemyjava/oop/MotorBikeRunner.java
@@ -0,0 +1,12 @@
+package com.udemyjava.oop;
+
+public class MotorBikeRunner
+{
+
+	public static void main(String[] args)
+	{
+		// TODO Auto-generated method stub
+
+	}
+
+}
