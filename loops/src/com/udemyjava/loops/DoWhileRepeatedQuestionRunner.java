66124a77fb77196304131a937f928f36d7d12765
takityaki <tataki26@gmail.com>
2022-08-30 21:45:37 +0900

    Add DoWhileRepeatedQuestionRunner class
    to practice do while loop

---
+++ loops/src/com/udemyjava/loops/DoWhileRepeatedQuestionRunner.java
@@ -0,0 +1,29 @@
+package com.udemyjava.loops;
+
+import java.util.Scanner;
+
+public class DoWhileRepeatedQuestionRunner
+{
+
+	public static void main(String[] args)
+	{
+		Scanner scanner = new Scanner(System.in);
+		
+		int number=-1;
+		
+		// you have to ask a question at least once
+		do
+		{
+			if (number != -1)
+				System.out.println("Cube is "+(number*number*number));
+			
+			System.out.print("Enter a number: ");
+			number = scanner.nextInt();
+			
+		} while(number>=0);
+		
+		System.out.println("Thank You! Have Fun!");
+		
+	}
+
+}
