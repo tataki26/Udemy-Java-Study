d78da761e22810ff1892eaa532970e1a1d2cfebb
takityaki <tataki26@gmail.com>
2022-08-30 21:25:53 +0900

    Add WhileNumberPlayer and WhileNumberPlayerRunner class
    to solve while loop problems

---
+++ loops/src/com/udemyjava/loops/WhileNumberPlayerRunner.java
@@ -0,0 +1,16 @@
+package com.udemyjava.loops;
+
+public class WhileNumberPlayerRunner
+{
+
+	public static void main(String[] args)
+	{
+		WhileNumberPlayer player = new WhileNumberPlayer(30);
+		
+		player.printSquaresUptoLimit();
+		
+		player.printCubesUptoLimit();
+
+	}
+
+}
