03ad123da00eb3116a3aca7a6b9b2ac4d3c503b7
takityaki <tataki26@gmail.com>
2022-09-01 22:47:38 +0900

    Add Student and StudentRunner class
    - to practice array and enhanced for loop

---
+++ introduction-to-array-and-arraylist/src/com/udemyjava/arrays/StudentRunner.java
@@ -0,0 +1,31 @@
+package com.udemyjava.arrays;
+
+import java.math.BigDecimal;
+
+public class StudentRunner
+{
+
+	public static void main(String[] args)
+	{
+		int[] marks = {80, 90, 100};
+		
+		Student student = new Student("Takityaki", marks);
+		
+		int number = student.getNumberOfMarks();
+		System.out.println("number of marks: "+number);
+		
+		int sum = student.getTotalSumOfMarks();
+		System.out.println("total sum of marks: "+sum);
+		
+		int maximumMark = student.getMaximumMark();
+		System.out.println("maximum of marks: "+maximumMark);
+		
+		int minimumMark = student.getMinimumMark();
+		System.out.println("minimum of marks: "+minimumMark);
+		
+		BigDecimal average = student.getAverageMarks();
+		System.out.println("average of marks: "+average);
+
+	}
+
+}
