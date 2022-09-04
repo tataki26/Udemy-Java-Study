b5f2a77f3fd18d719d63b256185ffb2c76aabd37
takityaki <tataki26@gmail.com>
2022-09-04 22:24:00 +0900

    Add Rectangle and RectangleRunner class
    - to solve object oriented programming problem

---
+++ object-oriented-programming-2/src/com/udemyjava/oop/level2/RectangleRunner.java
@@ -0,0 +1,15 @@
+package com.udemyjava.oop.level2;
+
+public class RectangleRunner
+{
+	public static void main(String[] args)
+	{
+		Rectangle rectangle = new Rectangle(12,23);
+		
+		System.out.println(rectangle);
+		
+		rectangle.setWidth(25);
+		System.out.println(rectangle);
+	}
+
+}
