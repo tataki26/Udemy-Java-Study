99ef7e97de773987600c2ce2522db29a1329b88d
takityaki <tataki26@gmail.com>
2022-09-04 20:39:55 +0900

    Update student class
    - Change marks array into variable argument

--- introduction-to-array-and-arraylist/src/com/udemyjava/arrays/Student.java
+++ introduction-to-array-and-arraylist/src/com/udemyjava/arrays/Student.java
@@ -10,7 +10,7 @@ public class Student
 	private String name;
 	private int[] marks;
 	
-	public Student(String name, int[] marks)
+	public Student(String name, int... marks)
 	{
 		this.name = name;
 		this.marks = marks;
