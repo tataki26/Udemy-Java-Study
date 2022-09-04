edd6cc74e5c35c38f13745bf540e19292a0beca5
takityaki <tataki26@gmail.com>
2022-09-04 22:06:39 +0900

    Add switchOn/Off and setSpeed method
    - to practice constructor which is easy for user

--- object-oriented-programming-2/src/com/udemyjava/oop/level2/Fan.java
+++ object-oriented-programming-2/src/com/udemyjava/oop/level2/Fan.java
@@ -18,9 +18,40 @@ public class Fan
 		this.color = color;
 	}
 	
+	// isOn
+	
+	// 1
+	/*
+	// not intuitive
+	public void isOn(boolean isOn)
+	{
+		this.isOn = isOn;
+	}
+	*/
+	
+	// 2
+	// easy for user
+	public void switchOn()
+	{
+		this.isOn = true;
+		setSpeed((byte)5);
+	}
+	
+	public void switchOff()
+	{
+		this.isOn = false;
+		setSpeed((byte)0);
+	}
+	
+	public void setSpeed(byte speed)
+	{
+		this.speed = speed;
+	}
+	
 	// print the state
 	public String toString() {
 		return String.format("make: %s, radius: %f, color: %s, isOn: %b, speed: %d",
 				make, radius, color, isOn, speed);
 	}
+
 }
--- object-oriented-programming-2/src/com/udemyjava/oop/level2/FanRunner.java
+++ object-oriented-programming-2/src/com/udemyjava/oop/level2/FanRunner.java
@@ -7,6 +7,13 @@ public class FanRunner
 	{
 		Fan fan = new Fan("Manufacturer 1", 0.34567, "Green");
 		
+		fan.switchOn();
+		System.out.println(fan);
+		
+		fan.setSpeed((byte)3);
+		System.out.println(fan);
+		
+		fan.switchOff();
 		System.out.println(fan);
 
 	}
