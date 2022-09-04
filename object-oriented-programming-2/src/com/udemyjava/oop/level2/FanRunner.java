fd994f2cde3c717cd9da7ee0320e49b96935627b
takityaki <tataki26@gmail.com>
2022-09-04 21:49:58 +0900

    Add Fan and FanRunner class
    - to practice object oriented programming

---
+++ object-oriented-programming-2/src/com/udemyjava/oop/level2/FanRunner.java
@@ -0,0 +1,14 @@
+package com.udemyjava.oop.level2;
+
+public class FanRunner
+{
+
+	public static void main(String[] args)
+	{
+		Fan fan = new Fan("Manufacturer 1", 0.34567, "Green");
+		
+		System.out.println(fan);
+
+	}
+
+}
