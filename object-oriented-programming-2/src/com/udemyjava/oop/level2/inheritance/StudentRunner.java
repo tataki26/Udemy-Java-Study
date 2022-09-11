bf24a43a89eaad1522bfffe99a0216cd2a8c02f4
takityaki <tataki26@gmail.com>
2022-09-11 18:03:55 +0900

    Update StudentRunner class
    - to learn Object class

--- object-oriented-programming-2/src/com/udemyjava/oop/level2/inheritance/StudentRunner.java
+++ object-oriented-programming-2/src/com/udemyjava/oop/level2/inheritance/StudentRunner.java
@@ -10,6 +10,15 @@ public class StudentRunner
 		// super class의 setter
 		student.setName("Takityaki");
 		student.setEmail("takoyaki2077@gmail.com");
+		
+		Person person = new Person();
+		
+		// Person에 정의되지 않은 메소드
+		// Person이 상속하고 있는 Object의 메소드
+		String value = person.toString();
+		
+		System.out.println(value);
+		System.out.println(person);
 
 	}
 
