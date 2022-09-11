e8ceddc86b7211e65b0bc9452fe028a9e8a8b0fb
takityaki <tataki26@gmail.com>
2022-09-11 17:57:47 +0900

    Add Person, Student and StudentRunner class
    - to practice how to apply the concept of inheritance

---
+++ object-oriented-programming-2/src/com/udemyjava/oop/level2/inheritance/StudentRunner.java
@@ -0,0 +1,16 @@
+package com.udemyjava.oop.level2.inheritance;
+
+public class StudentRunner
+{
+
+	public static void main(String[] args)
+	{
+		Student student = new Student();
+		
+		// super class의 setter
+		student.setName("Takityaki");
+		student.setEmail("takoyaki2077@gmail.com");
+
+	}
+
+}
