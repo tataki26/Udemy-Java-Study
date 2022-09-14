ea2d83908baa2a1f9309970c929528075ce75597
takityaki <tataki26@gmail.com>
2022-09-14 21:59:35 +0900

    Update GameRunner.java
    - Refactor GameRunner class using array and for loop

--- object-oriented-programming-2/src/com/udemyjava/oop/interfaces/GameRunner.java
+++ object-oriented-programming-2/src/com/udemyjava/oop/interfaces/GameRunner.java
@@ -5,6 +5,8 @@ public class GameRunner
 
 	public static void main(String[] args)
 	{
+		
+		/*
 		MarioGame mario = new MarioGame();
 		
 		mario.up();
@@ -25,6 +27,22 @@ public class GameRunner
 		game.down();
 		game.left();
 		game.right();
+		*/
+		
+		// 같은 코드
+		// 다형성
+		// 다른 두 클래스에서 호출
+		// 같은 코드가 두 개의 다른 행동 방식을 가진다
+		GamingConsole[] games = { new MarioGame(), new ChessGame() };
+		
+		for(GamingConsole game: games)
+		{
+			// 실행되는 코드는 같다
+			game.up();
+			game.down();
+			game.left();
+			game.right();
+		};
 
 	}
 
