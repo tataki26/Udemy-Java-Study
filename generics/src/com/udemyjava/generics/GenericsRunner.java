6e04c6c06df0348e0f03eebf83f61f6ee1278291
takityaki <tataki26@gmail.com>
2022-09-25 21:10:04 +0900

    Update GenericsRunner.java
    - to learn how to make generic method

--- generics/src/com/udemyjava/generics/GenericsRunner.java
+++ generics/src/com/udemyjava/generics/GenericsRunner.java
@@ -1,12 +1,39 @@
 package com.udemyjava.generics;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
+
 import com.udemyjava.generics.MyCustomList;
 
 public class GenericsRunner
 {
+	
+	static <X> X doubleValue(X value)
+	{
+		return value;
+	}
+	
+	static <X extends List> void duplicate(X list)
+	{
+		list.addAll(list);
+	}
 
 	public static void main(String[] args)
 	{
+		String strVal = doubleValue(new String());
+		Integer intVal = doubleValue(Integer.valueOf(5));
+		ArrayList arrVal = doubleValue(new ArrayList());
+		
+		ArrayList<Integer> arrNums = new ArrayList<>(Arrays.asList(1,2,3));
+		duplicate(arrNums);
+		System.out.println(arrNums);
+		
+		LinkedList<Integer> linkedNums = new LinkedList<>(Arrays.asList(1,2,3));
+		duplicate(linkedNums);
+		System.out.println(linkedNums);
+		
 		MyCustomList<String> list = new MyCustomList<>();
 		
 		list.addElement("Element1");
