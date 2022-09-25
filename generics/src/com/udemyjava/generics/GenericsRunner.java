b6fb11f632f17de49f3a0cd091d6f4b42e8200f4
takityaki <tataki26@gmail.com>
2022-09-25 18:33:57 +0900

    Update GenericsRunner and MyCustomList class
    - to learn how to use generics

--- generics/src/com/udemyjava/generics/GenericsRunner.java
+++ generics/src/com/udemyjava/generics/GenericsRunner.java
@@ -7,17 +7,19 @@ public class GenericsRunner
 
 	public static void main(String[] args)
 	{
-		// ArrayList<String> 
-		MyCustomList list = new MyCustomList();
+		MyCustomList<String> list = new MyCustomList<>();
 		
 		list.addElement("Element1");
 		list.addElement("Element2");
 		
-		// is it possible to add Integer in list2?
-		MyCustomList list2 = new MyCustomList();
+		System.out.println(list);
+		
+		MyCustomList<Integer> list2 = new MyCustomList<>();
 		list2.addElement(Integer.valueOf(5));
 		list2.addElement(Integer.valueOf(7));
-
+		
+		System.out.println(list2);
+	
 	}
 
 }
