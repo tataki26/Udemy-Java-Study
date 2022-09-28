817add4ece3534de8f9ebf1aadac44c2ffd5f2ee
takityaki <tataki26@gmail.com>
2022-09-28 23:02:13 +0900

    Create MethodReferencesRunner.java
    - to learn the concept of method reference

---
+++ functional-programming/src/com/udemyjava/functionalprogramming/MethodReferencesRunner.java
@@ -0,0 +1,45 @@
+package com.udemyjava.functionalprogramming;
+
+import java.util.Arrays;
+
+public class MethodReferencesRunner
+{
+
+	public static void print(Integer number)
+	{
+		System.out.println(number);
+	}
+	
+	public static boolean isEven(Integer number)
+	{
+		return number % 2 == 0;
+	}
+	
+	public static void main(String[] args)
+	{
+		Arrays.asList("Ant", "Bat", "Cat", "Dog", "Elephant").stream()
+			.map(s -> s.length())
+			//.forEach(s -> System.out.println(s));
+			//.forEach(System.out::println);
+			.forEach(s->MethodReferencesRunner.print(s));
+		
+		Arrays.asList("Ant", "Bat", "Cat", "Dog", "Elephant").stream()
+		//.map(s -> s.length())
+		.map(String::length)
+		.forEach(MethodReferencesRunner::print);
+		
+		Integer max = Arrays.asList(23,45,67,34).stream()
+				.filter(n -> n%2 == 0)
+				.max((n1,n2) -> Integer.compare(n1,n2))
+				.orElse(0);
+		
+		Integer max = Arrays.asList(23,45,67,34).stream()
+							.filter(MethodReferencesRunner::isEven)
+							.max(Integer::compare)
+							.orElse(0);
+		
+		System.out.println(max);
+
+	}
+
+}
