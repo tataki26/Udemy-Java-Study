bf679605291bc5108b240d2883b09d953610927d
takityaki <tataki26@gmail.com>
2022-09-28 22:41:44 +0900

    Update LambdaBehindTheScreenRunner.java
    - to implement Function interface

--- functional-programming/src/com/udemyjava/functionalprogramming/LambdaBehindTheScreenRunner.java
+++ functional-programming/src/com/udemyjava/functionalprogramming/LambdaBehindTheScreenRunner.java
@@ -2,6 +2,7 @@ package com.udemyjava.functionalprogramming;
 
 import java.util.Arrays;
 import java.util.function.Consumer;
+import java.util.function.Function;
 import java.util.function.Predicate;
 
 class EvenNumberPredicate implements Predicate<Integer>
@@ -11,6 +12,17 @@ class EvenNumberPredicate implements Predicate<Integer>
 	public boolean test(Integer number)
 	{
 		return number % 2 == 0;
+	}
+	
+}
+
+class NumberSquareMapper implements Function<Integer, Integer>
+{
+
+	@Override
+	public Integer apply(Integer number)
+	{
+		return number * number;
 	}
 	
 }
@@ -35,6 +47,8 @@ public class LambdaBehindTheScreenRunner
 		Arrays.asList(23,43,34,45,36,48).stream()
 			// .filter(n -> n % 2 == 0)
 			.filter(new EvenNumberPredicate())
+			// .map(n -> n * n)
+			.map(new NumberSquareMapper())
 			//.forEach(e -> System.out.println(e));
 			.forEach(new SystemOutConsumer());
 
@@ -45,6 +59,10 @@ public class LambdaBehindTheScreenRunner
 		// .forEach()
 		// Consumer<? super T> action
 		// void accept(T t);
+		
+		// .map()
+		// <R> Stream<R> map(Function<? super T, ? extends R> mapper);
+		// R apply(T t);
 	}
 
 }
