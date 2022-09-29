0dc2f0a6865ec2074f546ef829d958adefde4c1e
takityaki <tataki26@gmail.com>
2022-09-29 22:03:56 +0900

    Update MethodReferencesRunner.java
    - to learn the status of function in funtional programming

--- functional-programming/src/com/udemyjava/functionalprogramming/MethodReferencesRunner.java
+++ functional-programming/src/com/udemyjava/functionalprogramming/MethodReferencesRunner.java
@@ -1,6 +1,7 @@
 package com.udemyjava.functionalprogramming;
 
 import java.util.Arrays;
+import java.util.function.Predicate;
 
 public class MethodReferencesRunner
 {
@@ -12,7 +13,13 @@ public class MethodReferencesRunner
 	
 	public static boolean isEven(Integer number)
 	{
+		// 2. returning functions from methods
 		return number % 2 == 0;
+	}
+	
+	private static Predicate<? super Integer> createEvenPredicate()
+	{
+		return n -> n%2 == 0;
 	}
 	
 	public static void main(String[] args)
@@ -28,11 +35,19 @@ public class MethodReferencesRunner
 		.map(String::length)
 		.forEach(MethodReferencesRunner::print);
 		
+		// 1. Storing functions in variables
+		// Predicate<? super Integer> evenPredicate = n -> n%2 == 0;
+		// Predicate<? super Integer> oddPredicate = n -> n%2 == 1;
+		
+		// 2.
+		Predicate<? super Integer> evenPredicate = createEvenPredicate();
+		
 		Integer max1 = Arrays.asList(23,45,67,34).stream()
-				.filter(n -> n%2 == 0)
+				.filter(evenPredicate)
 				.max((n1,n2) -> Integer.compare(n1,n2))
 				.orElse(0);
 		
+		// 3. passing function to methods
 		Integer max2 = Arrays.asList(23,45,67,34).stream()
 							.filter(MethodReferencesRunner::isEven)
 							.max(Integer::compare)
