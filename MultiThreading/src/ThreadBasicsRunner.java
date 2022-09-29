b68e6a36b4c087f1d9458e08a265629a8cbbf8a0
takityaki <tataki26@gmail.com>
2022-09-29 22:30:25 +0900

    Update ThreadBasicsRunner.java
    - to learn how to use Thread with Thread class inheritance

--- MultiThreading/src/ThreadBasicsRunner.java
+++ MultiThreading/src/ThreadBasicsRunner.java
@@ -1,10 +1,9 @@
-
-public class ThreadBasicsRunner
+class Task1 extends Thread
 {
-
-	public static void main(String[] args)
+	public void run() // signature
 	{
-		// Task1
+		System.out.println("\nTask1 Started");
+		
 		for(int i=101;i<=199;i++)
 		{
 			System.out.print(i+" ");
@@ -13,7 +12,22 @@ public class ThreadBasicsRunner
 				System.out.print("\n");
 		}
 		
-		System.out.print("\nTask1 Done ");
+		System.out.println("\nTask1 Done");
+	}
+}
+
+public class ThreadBasicsRunner
+{
+	
+	public static void main(String[] args)
+	{
+		System.out.print("\nTask1 Kicked Off");
+		
+		// Task1 - 101 to 199
+		Task1 task1 = new Task1();
+		task1.start();
+		
+		System.out.print("\nTask2 Kicked Off");
 		
 		// Task2
 		for(int i=201;i<=299;i++)
@@ -24,7 +38,9 @@ public class ThreadBasicsRunner
 				System.out.print("\n");
 		}
 		
-		System.out.print("\nTask2 Done ");
+		System.out.println("\nTask2 Done");
+		
+		System.out.print("\nTask3 Kicked Off");
 		
 		// Task3
 		for(int i=301;i<=399;i++)
