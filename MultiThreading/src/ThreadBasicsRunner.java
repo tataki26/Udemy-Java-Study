1c301193903df81fd683123336252f08f897978e
takityaki <tataki26@gmail.com>
2022-09-29 22:19:10 +0900

    Create ThreadBasicsRunner class
    - to learn the necessity of Thread

---
+++ MultiThreading/src/ThreadBasicsRunner.java
@@ -0,0 +1,44 @@
+
+public class ThreadBasicsRunner
+{
+
+	public static void main(String[] args)
+	{
+		// Task1
+		for(int i=101;i<=199;i++)
+		{
+			System.out.print(i+" ");
+			
+			if(i%10 == 0)
+				System.out.print("\n");
+		}
+		
+		System.out.print("\nTask1 Done ");
+		
+		// Task2
+		for(int i=201;i<=299;i++)
+		{
+			System.out.print(i+" ");
+			
+			if(i%10 == 0)
+				System.out.print("\n");
+		}
+		
+		System.out.print("\nTask2 Done ");
+		
+		// Task3
+		for(int i=301;i<=399;i++)
+		{
+			System.out.print(i+" ");
+			
+			if(i%10 == 0)
+				System.out.print("\n");
+		}
+		
+		System.out.print("\nTask3 Done ");
+		
+		System.out.print("\nMain Done");
+
+	}
+
+}
