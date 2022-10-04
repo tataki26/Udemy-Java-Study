1c3b95fa80ce5edbafbd725a05575412d7ba7df0
takityaki <tataki26@gmail.com>
2022-10-04 22:45:34 +0900

    Create MultipleAnyCallableRunner.java
    - to learn how to use invokeAny

---
+++ MultiThreading/src/MultipleAnyCallableRunner.java
@@ -0,0 +1,30 @@
+import java.util.Arrays;
+import java.util.List;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+
+public class MultipleAnyCallableRunner
+{
+
+	public static void main(String[] args) throws InterruptedException, ExecutionException
+	{
+		// newFixedThreadPool의 인자에 따라 실행 속도 달라짐
+		// 한 번에 실행할 수 있는 Thread의 개수
+		ExecutorService executorService = Executors.newFixedThreadPool(3);
+		
+		// task lists
+		List<CallableTask> tasks = Arrays.asList(new CallableTask("Takityaki"), 
+				new CallableTask("Taki"), new CallableTask("tyaki"));
+		
+		// invoke any: return fastest future
+		String result = executorService.invokeAny(tasks);
+		
+		System.out.println(result);
+		
+		executorService.shutdown();
+
+	}
+
+}
