7a648626803899de213834202ac0bc066633f9e0
takityaki <tataki26@gmail.com>
2022-10-04 22:39:18 +0900

    Create MultipleCallableRunner.java
    - to learn how to use multiple callable thread

---
+++ MultiThreading/src/MultipleCallableRunner.java
@@ -0,0 +1,32 @@
+import java.util.Arrays;
+import java.util.List;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+
+public class MultipleCallableRunner
+{
+
+	public static void main(String[] args) throws InterruptedException, ExecutionException
+	{
+		// newFixedThreadPool의 인자에 따라 실행 속도 달라짐
+		// 한 번에 실행할 수 있는 Thread의 개수
+		ExecutorService executorService = Executors.newFixedThreadPool(1);
+		
+		// task lists
+		List<CallableTask> tasks = Arrays.asList(new CallableTask("Takityaki"), 
+				new CallableTask("Taki"), new CallableTask("tyaki"));
+		
+		// invoke all: return future list
+		List<Future<String>> results = executorService.invokeAll(tasks);
+		
+		for(Future<String> result:results) {
+			System.out.println(result.get());
+		}
+		
+		executorService.shutdown();
+
+	}
+
+}
