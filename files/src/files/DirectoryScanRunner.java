80a6985241afdb5f23cff05b684794ced9750cda
takityaki <tataki26@gmail.com>
2022-10-09 19:11:24 +0900

    Update DirectoryScanRunner.java
    - to learn how to get the list of files and directory with filtering

--- files/src/files/DirectoryScanRunner.java
+++ files/src/files/DirectoryScanRunner.java
@@ -2,16 +2,44 @@ package files;
 
 import java.io.IOException;
 import java.nio.file.Files;
+import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.nio.file.attribute.BasicFileAttributes;
+import java.util.function.BiPredicate;
+import java.util.function.Predicate;
+import java.util.stream.Stream;
 
 public class DirectoryScanRunner
 {
 
 	public static void main(String[] args) throws IOException
 	{
+		Path currentDirectory = Paths.get(".");
+		
 		// Functional Programming
 		// 현재 위치의 파일 및 디렉토리 목록을 출력한다
-		Files.list(Paths.get(".")).forEach(System.out::println);
+		// Files.list(currentDirectory).forEach(System.out::println);
+		
+		// Files.walk(currentDirectory, 2).forEach(System.out::println);
+		
+		// 1
+		// only java files
+		// check path and whether it contains ".java" or not
+		Predicate<? super Path> predicate = path -> String.valueOf(path).contains(".java");
+		
+		// Files.walk(currentDirectory, 4).filter(predicate).forEach(System.out::println);
+		
+		//2
+		// only java files
+		BiPredicate<Path, BasicFileAttributes> javaMatcher
+		= (path,attributes) -> String.valueOf(path).contains(".java");
+
+		// only directory
+		BiPredicate<Path, BasicFileAttributes> directoryMathcer
+		= (path,attributes) -> attributes.isDirectory();
+		
+		Files.find(currentDirectory, 4, directoryMathcer)
+		.forEach(System.out::println);
 
 	}
 
