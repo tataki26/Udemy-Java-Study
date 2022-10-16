945d6e4e058962b89159411023dcf463a56b0943
takityaki <tataki26@gmail.com>
2022-10-16 22:55:34 +0900

    Create ClassAccessModifiers.java and ClassAccessModifiersRunnerInOtherPackage.java
    - to learn class access modifiers

---
+++ tips-and-tricks/src/com/udemyjava/tips/access/package1/ClassAccessModifiers.java
@@ -0,0 +1,17 @@
+package com.udemyjava.tips.access.package1;
+
+// public, protected, (default), private
+// for encapsulation
+// public: able to use anywhere
+// default: only in same package
+// protected, private: not able to be used with class
+public class ClassAccessModifiers
+{
+
+	public static void main(String[] args)
+	{
+		ClassAccessModifiers c = new ClassAccessModifiers();
+
+	}
+
+}
---
+++ tips-and-tricks/src/com/udemyjava/tips/access/package2/ClassAccessModifiersRunnerInOtherPackage.java
@@ -0,0 +1,13 @@
+package com.udemyjava.tips.access.package2;
+
+import com.udemyjava.tips.access.package1.ClassAccessModifiers;
+
+public class ClassAccessModifiersRunnerInOtherPackage
+{
+	// ClassAccessModifiers: public class (visible in other class)
+	public static void main(String[] args)
+	{
+		ClassAccessModifiers c = new ClassAccessModifiers();
+	}
+
+}
